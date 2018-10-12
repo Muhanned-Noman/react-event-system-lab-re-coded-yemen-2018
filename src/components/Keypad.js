52d64272f3d647657aa54e711c69a36dd27ecb72
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 15:51:39 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -8,7 +8,7 @@ class Keypad extends React.Component {
  
   render() {
     return (
-      <button onKeyUp ={this.Keypad}>Tickle me!</button>
+      <button onKeyUp ={this.Keypad} />
     );
   }
 }
